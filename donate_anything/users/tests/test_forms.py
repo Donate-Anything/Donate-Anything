fd58ba2b69f90e1bcbeea4dbc3598a61e8921cd7
Andrew-Chen-Wang <acwangpython@gmail.com>
2020-08-15 14:36:54 -0400

    Added terms to test_forms in user app * I need to add some pre-commit that allows me to test before committing...

--- donate_anything/users/tests/test_forms.py
+++ donate_anything/users/tests/test_forms.py
@@ -19,6 +19,7 @@ class TestUserCreationForm:
                 "password1": proto_user._password,
                 "password2": proto_user._password,
                 "g-recaptcha-response": "PASSED",
+                "terms": True,
             }
         )
 
@@ -37,9 +38,42 @@ class TestUserCreationForm:
                 "password1": proto_user._password,
                 "password2": proto_user._password,
                 "g-recaptcha-response": "PASSED",
+                "terms": True,
             }
         )
 
         assert not form.is_valid()
         assert len(form.errors) == 1
         assert "username" in form.errors
+
+    def test_clean_terms(self):
+        # A user with proto_user params does not exist yet.
+        proto_user = UserFactory.build()
+
+        form = UserCreationForm(
+            {
+                "email": proto_user.email,
+                "username": proto_user.username,
+                "password1": proto_user._password,
+                "password2": proto_user._password,
+                "g-recaptcha-response": "PASSED",
+                "terms": False,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "terms" in form.errors
+
+        form = UserCreationForm(
+            {
+                "email": proto_user.email,
+                "username": proto_user.username,
+                "password1": proto_user._password,
+                "password2": proto_user._password,
+                "g-recaptcha-response": "PASSED",
+                "terms": True,
+            }
+        )
+
+        assert form.is_valid()
