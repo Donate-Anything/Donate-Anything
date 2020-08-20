56ff021c998611bf15d00ced7af83e38f12e54ca
Andrew-Chen-Wang <acwangpython@gmail.com>
2020-08-20 18:30:06 -0400

    Make signup.html less confusing for VerifiedAccount * Got off the phone with someone and received enough emails to think that the VerifiedAccount thing wasn't clear enough to people * markdown_editor.js needed a declaration of the converter variable... per usual somehow always forgetting it

--- donate_anything/static/js/markdown_editor.js
+++ donate_anything/static/js/markdown_editor.js
@@ -2,7 +2,7 @@ $("textarea").on('input', function () {
     // Check if it's the actual textarea
     // Ignore if it's just showdown itself
     if (this.id.slice(this.id.length - 9) !== "-showdown") {
-        converter = new showdown.Converter();
+        let converter = new showdown.Converter();
         converter.setFlavor('github');
         const text = $(this).val(),
             target = document.getElementById(this.id.toString().concat("-showdown"));
