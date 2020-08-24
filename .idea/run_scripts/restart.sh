5acc7b2367b42a1a1f3f9af8e89e38cc8f695acd
Andrew-Chen-Wang <acwangpython@gmail.com>
2020-08-24 13:30:39 -0400

    Made migrate a call_command in restart.sh

--- .idea/run_scripts/restart.sh
+++ .idea/run_scripts/restart.sh
@@ -1,6 +1,5 @@
 # Removing the volume because we might be creating new tables or modifying old ones
 docker-compose -f local.yml down --volumes
-docker-compose -f local.yml run --rm django python manage.py migrate
 echo "Creating username test, pw test, email test@test.com with some other test data"
-docker-compose -f local.yml run --rm django python manage.py shell -c "from django.core.management import call_command; call_command('create_item_data'); call_command('create_charity_data'); call_command('create_forum_data'); from django.contrib.auth import get_user_model; user = get_user_model().objects.create_superuser('test', 'test@test.com', 'test'); from allauth.account.models import EmailAddress; EmailAddress.objects.create(user=user, email=user.email, verified=True, primary=True)"
+docker-compose -f local.yml run --rm django python manage.py shell -c "from django.core.management import call_command; call_command('migrate'); call_command('create_item_data'); call_command('create_charity_data'); call_command('create_forum_data'); from django.contrib.auth import get_user_model; user = get_user_model().objects.create_superuser('test', 'test@test.com', 'test'); from allauth.account.models import EmailAddress; EmailAddress.objects.create(user=user, email=user.email, verified=True, primary=True)"
 echo "Done: username test, pw test, email test@test.com"
