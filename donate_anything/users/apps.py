2f2a5a84ba6cea0c0eebe02c34ada24fabd8647e
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2020-11-24 02:44:37 -0500

    Allow django-ratelimit to use Axes+Ipware for ip
    * First used for Velnota. Initial testing for throttling using django ratelimit
    * The following gist Andrew made goes a little more in detail to this implementation: https://gist.github.com/Andrew-Chen-Wang/bd30e31211ec8c1046886877e137cdf5

--- donate_anything/users/apps.py
+++ donate_anything/users/apps.py
@@ -7,7 +7,22 @@ class UsersConfig(AppConfig):
     verbose_name = _("Users")
 
     def ready(self):
+        patch_rate_limit()
         try:
             import donate_anything.users.signals  # noqa F401
         except ImportError:
             pass
+
+
+# noinspection PyPackageRequirements,PyProtectedMember
+def patch_rate_limit():
+    from axes.utils import get_client_ip_address
+    from ratelimit.core import _SIMPLE_KEYS, ip_mask
+
+    def user_or_ip(request):
+        if request.user.is_authenticated:
+            return str(request.user.pk)
+        return ip_mask(get_client_ip_address(request))
+
+    _SIMPLE_KEYS["ip"] = lambda r: ip_mask(get_client_ip_address(r))
+    _SIMPLE_KEYS["user_or_ip"] = user_or_ip
